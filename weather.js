bbf16a32c3cb03219d5bebb9e9c8e638cfa5c0fa
zhxu33 <77419802+zhxu33@users.noreply.github.com>
2022-12-20 02:07:27 -0800

    update

--- weather.js
+++ weather.js
@@ -5,36 +5,23 @@ if (Array.isArray(savedCityList)) {
     cityList = savedCityList
 } else {
     cityList = [
-        { name: 'Davis',
-        date: '2022-11-12',
-        id: 'id1'
-        },
-        { name: 'San Francisco',
-        date: '2022-11-12',
-        id: 'id2'
-        },
+        "San Francisco", "Davis"
     ]
 }
 render();
 
-//b973b62226fb5128e50d6ab73540ec56
-//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={b973b62226fb5128e50d6ab73540ec56}
 // Creates a city
-function createCity(name, date, id) {
-    cityList.push( {
-        name: name,
-        date: date,
-        id: ''+id
-    })
+function createCity(name) {
+    cityList.push(name)
     saveCities();
 }
 
 //Delete a city
-function removeCity(idToDelete) {
+function removeCity(name) {
     cityList = cityList.filter(function(city) {
         //if id is equal city's return false and delete
         //return true otherwise and do nothing
-        return !(city.id == idToDelete);
+        return !(city == name);
     });
     saveCities();
 }
@@ -44,27 +31,17 @@ function saveCities() {
 }
 
 //controller
-function addCity() {
-    let cityText = document.getElementById('search-city').value;
-    cityText = cityText.charAt(0).toUpperCase() + cityText.slice(1);
-    const date = new Date();
-    const id = date.getTime();
-    createCity(cityText, date.getFullYear()+'/'+(date.getMonth()+1)+"/"+date.getDate(), id)
-    render();
-}
-
-function findCity() {
-
-    cityList = cityList.filter(function(city) {
-        //if id is equal city's return false and delete
-        //return true otherwise and do nothing
-        return !(city.id == idToDelete);
-    });
+function addCity(cityText) {
+    if (cityList.includes(cityText) == false) {
+        createCity(cityText);
+        render();
+    }
 }
 
 let cityP = document.getElementById('city');
 let tempP = document.getElementById('temp');
 let weatherP = document.getElementById('weather');
+let tempstatP = document.getElementById('tempstat');
 function searchCity(find) {
     let cityText = document.getElementById('search-city').value;
     if (find != null) {
@@ -80,65 +57,86 @@ function searchCity(find) {
     })
     .then(data => {
         if (find == null) {
-            addCity();
+            addCity(data['name']);
         }
         cityP.innerText = data['name'];
-        tempP.innerText = Math.round(data['main']['temp'])+'°';//+' °F';
+        tempP.innerText = Math.round(data['main']['temp'])+'°';
         weatherP.innerText = data['weather'][0]['main'];
+        tempstatP.innerText = "L:"+Math.round(data['main']['temp_min'])+'° '+ "H:"+Math.round(data['main']['temp_max'])+'°';
+        document.body.style.backgroundImage = "url('"+ "https://source.unsplash.com/1600x900/?"+cityText +"%20city')";
     });
 }
 
 function deleteCity(event) {
-    const deletebutton = event.target;
-    const idToDelete = deletebutton.id;
-    removeCity(idToDelete);
+    const parent = event.target.parentElement;
+    const cityName = parent.getElementsByClassName("sample-city")[0];
+    removeCity(cityName.textContent);
+    parent.remove();
     render();
 }
 
 function viewCity(event) {
-    const viewButton = event.target;
-    const idToView = viewButton.id;
-    cityList.forEach(function (cityTitle) {
-        if (cityTitle.id == idToView) {
-            searchCity(cityTitle.name);
-        }
-    });
+    const parent = event.target.parentElement;
+    const cityName = parent.getElementsByClassName("sample-city")[0];
+    searchCity(cityName.textContent);
+}
+
+function starCity(event) {
+    const parent = event.target.parentElement;
+    const cityName = parent.getElementsByClassName("sample-city")[0];
+    removeCity(cityName.textContent);
+    cityList.unshift(cityName.textContent);
+    render();
 }
 
 function clearHistory() {
     cityList = [];
+    saveCities()
     render();
 }
 
 // View
 function render() { //reset list of cities
-    document.getElementById('city-list').innerHTML = '';
-    const clearButton = document.createElement('button');
-    clearButton.innerText = 'Clear';
-    clearButton.style = 'margin-left: 12px';
-    clearButton.onclick = clearHistory;
-    clearButton.className = "special-button"
-    document.getElementById('city-list').appendChild(clearButton)
+    const list = document.getElementById('city-list');
+    const sample = document.getElementById("samp");
+    while (list.lastElementChild) {
+        if (list.lastElementChild.className == "sample") {
+            list.removeChild(list.lastElementChild);
+        } else {
+            break;
+        }
+      }
+    document.getElementById('clear-button').onclick = clearHistory;
     cityList.forEach(function (cityTitle) {
-        const element = document.createElement('div');
-        const deleteButton = document.createElement('button');
-        deleteButton.innerText = 'Delete';
-        deleteButton.style = 'margin-left: 12px';
-        deleteButton.id = cityTitle.id;
-        deleteButton.className = "special-button";
-        element.appendChild(deleteButton);
-        deleteButton.onclick = deleteCity;
-        const text = document.createElement('h');
-        text.style = 'margin-left: 12px';
-        text.innerText = cityTitle.name + ' ' + cityTitle.date
-        element.appendChild(text);
-        const viewButton = document.createElement('button');
-        viewButton.innerText = 'View';
-        viewButton.style = 'margin-left: 12px';
-        viewButton.id = cityTitle.id;
-        viewButton.className = "special-button";
-        element.appendChild(viewButton);
-        viewButton.onclick = viewCity;
-        document.getElementById('city-list').appendChild(element)
+        let element = sample.cloneNode(true); 
+        element.removeAttribute('id');
+        element.className = "sample";
+        element.getElementsByClassName("sample-delete")[0].onclick = deleteCity;
+        element.getElementsByClassName("sample-view")[0].onclick = viewCity;
+        element.getElementsByClassName("sample-star")[0].onclick = starCity;
+        element.getElementsByClassName("sample-city")[0].textContent = cityTitle;
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityTitle+"&units=imperial"+'&appid=b973b62226fb5128e50d6ab73540ec56')
+        .then((response) => {
+        if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+        }
+            return response.json();
+        })
+        .then(data => {
+            element.getElementsByClassName("sample-temp")[0].innerText = Math.round(data['main']['temp'])+'°';
+            element.getElementsByClassName("sample-weather")[0].innerText = data['weather'][0]['main'];
+        });
+        document.getElementById('city-list').appendChild(element);
     });
 }
+
+document.addEventListener('readystatechange', () => {    
+    if (document.readyState == 'complete') {
+        if (cityList[0]) {
+            searchCity(cityList[0]);
+        } else {
+            searchCity("San Francisco");
+        }
+    }
+});
